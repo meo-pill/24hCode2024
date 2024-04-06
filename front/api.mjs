80f1e7f3ab960f9ef9a294917715c08401624fba
meo <83820503+meo-pill@users.noreply.github.com>
2024-04-06 11:21:52 +0000

    fix

--- front/api.mjs
+++ front/api.mjs
@@ -1,31 +1,38 @@
 import fetch from "node-fetch";
 
 class Api {
-  async listEquipe() {}
+	url = `http://149.202.79.34:8085/api/canvas/${canvasId}`;
 
-  async getEquipeDetails() {}
+	socket = io('ws://149.202.79.34:8085/api/socket', {
+		auth: {
+			token: '%K7%%GG,2U5g-k9'
+		}
+	});
 
-  async getCanvaData(canvasId, accessToken) {
-      const url = `http://149.202.79.34:8085/api/canvas/${canvasId}`;
-      try {
-        const response = await fetch(url, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-        if (!response.ok) throw new Error(`Error: ${response.statusText}`);
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        console.error("Erreur:", error);
-        return null;
-      }
-  }
+	async listEquipe() { }
 
-  async getCanvaSettings() {}
+	async getEquipeDetails() { }
 
-  async getWorkerDetails() {}
+	async getCanvaData(canvasId, accessToken) {
+		try {
+			const response = await fetch(url, {
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+			});
+			if (!response.ok) throw new Error(`Error: ${response.statusText}`);
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			console.error("Erreur:", error);
+			return null;
+		}
+	}
 
-  async setWorkerPosition() {}
+	async getCanvaSettings() { }
+
+	async getWorkerDetails() { }
+
+	async setWorkerPosition() { }
 }
