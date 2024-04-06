cf6a2cd6b6111b94eb51bc51672fbcca3e45a22c
Aytixel <aytixel@gmail.com>
2024-04-06 19:54:47 +0200

    Update

--- front/worker.mjs
+++ front/worker.mjs
@@ -6,6 +6,8 @@ const api = new Api()
 
 await api.init()
 
+const chunk_id = 5
+const nb_worker = 50
 const timeout = 11000
 const size = 50
 const line = 2
@@ -13,9 +15,21 @@ const pixel = []
 
 for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
-        setTimeout(() => {
-            api.setWorkerPosition(351 + j, 5, output[i][j], i, j - 50 * -(line - 4))
-            console.log(351 + j, output[i][j], i, j - 50 * -(line - 4))
-        }, timeout * i + j * (timeout / size))
+        if (output[i][j])
+            pixel.push([i, j, output[i][j]])
     }
 }
+
+function update() {
+    for (let i = 0; i < nb_worker; i++) {
+        setTimeout(() => {
+            const [x, y, color] = pixel[Math.round(Math.random() * (pixel.length - 1))]
+
+            api.setWorkerPosition(351 + i, chunk_id, color, x, y - 50 * -(line - 4))
+            console.log(351 + i, chunk_id, color, x, y - 50 * -(line - 4))
+        }, (timeout / nb_worker) * i)
+    }
+}
+
+setInterval(update, timeout)
+update()
