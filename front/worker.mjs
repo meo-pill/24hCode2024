724cb2462f68ebdd9670fef2a9b1851cd48a6d4a
Aytixel <aytixel@gmail.com>
2024-04-06 15:41:34 +0200

    Update worker.mjs

--- front/worker.mjs
+++ front/worker.mjs
@@ -2,7 +2,7 @@ import { Api } from "./api.mjs"
 
 const api = new Api()
 
-for (let i = 10; i < 50; i++) {
+for (let i = 0; i < 50; i++) {
     setTimeout(() => {
         for (let j = 0; j < 50; j++) {
             api.setWorkerPosition(351 + j, 5, "golden_yellow", i, j)
